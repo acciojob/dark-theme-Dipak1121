bbb4608296c65ca75348820a6b29aca409e61053
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-04 12:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+let mainDiv = document.getElementById("app");
+let btn = document.getElementById("swap");
+btn.addEventListener("click", onClick);
+function onClick(){
+	mainDiv.classList.toggle("night");
+	btn.classList.toggle("button_night")
+}
